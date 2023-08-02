3f3e72ba1646f3fe26fc871d497bee4192b3c9cb
HDUY <leduy020702@gmail.com>
2023-08-02 13:14:03 +0700

    Do_An_Tritruenhantao

--- DOAN/SuyDienTien.py
+++ DOAN/SuyDienTien.py
@@ -31,38 +31,23 @@ class Node:
 
 
 def law_of_syllogism(graph, start, dest):
-    """
-    Law of syllogism
-
-    1. P -> Q
-    2. Q -> R
-    3. Therefore: P -> R
-
-    Using Breath First Search algorithm to perform Law of syllogism
-    """
 
     queue = []
     tree = {}
 
-    # Flag to check have path from start to dest
     flag = None
 
-    # List for vertices visited
     visited = []
 
     # Create BFS tree
     tree[start] = Node(start)
 
-    # Mark the start node as visited and enqueue it
     queue.append(start)
     visited.append(start)
 
     while queue:
-        # Dequeue a vertex from queue
         s = queue.pop(0)
 
-        # Get all adjacent vertices of the dequeue vertex s.
-        # If a adjacent has not been visited, then mark it visited and enqueue it
         for i in graph[s]:
             if i not in visited:
                 queue.append(i)
@@ -71,7 +56,6 @@ def law_of_syllogism(graph, start, dest):
                 tree[i] = Node(i)
                 tree[i].parent = tree[s]
 
-    # Check the path to destination
     if dest not in tree.keys():
         flag = False
     else:
@@ -81,15 +65,6 @@ def law_of_syllogism(graph, start, dest):
 
 
 def modus_ponens(graph, arg1, arg2, antecedent_list):
-    """
-    Modus ponens
-
-    1. arg1 -> arg2 (First premise is a conditional statement)
-    2. arg1 (Second premise is the antecedent)
-    3. arg2 (Conclusion deduced is the consequent)
-    """
-
-    # String for first and second premise
     premise = '1. ' + arg1 + ' -> ' + arg2 + '\n' + \
         '2. ' + arg1 + '\n' + '-' * 20 + '\n' + '3. '
 
@@ -103,15 +78,6 @@ def modus_ponens(graph, arg1, arg2, antecedent_list):
 
 
 def modus_tollens(graph, arg1, arg2, negative_consequent_list):
-    """
-    Modus tollens
-
-    1. arg1 -> arg2 (First premise is a conditional statement)
-    2. ¬arg2 (Second premise is the negation of the consequent)
-    3. ¬arg1 (Conclusion deduced is the negation of the antecedent)
-    """
-
-    # String for first and second premise
     premise = '1. ' + arg1 + ' -> ' + arg2 + '\n' + \
         '2. ¬' + arg2 + '\n' + '-' * 20 + '\n' + '3. '
 
@@ -125,19 +91,13 @@ def modus_tollens(graph, arg1, arg2, negative_consequent_list):
 
 
 if __name__ == '__main__':
-    # Create first premise
     g = Graph()
     g.add_condition_statement('P', 'Q')
     g.add_condition_statement('Q', 'R')
-    # g.print_graph()
-
-    # Create list of antecedent
     ant_list = ['P', 'Q']
 
-    # Create list of the negation of the consequent
     cons_list = ['Q', 'R']
 
-    # Deduction Reasoning
     conclusion1 = modus_ponens(g.get_graph(), 'P', 'R', ant_list)
     print(conclusion1)
 
